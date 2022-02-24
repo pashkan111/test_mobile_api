d0649a3c85fbfa5d6f166bbae4182e523e314512
pashkan111 <kozhevnikov0001@yandex.ru>
2022-02-24 15:44:12 +0300

    set up admin and added str method to models

--- mobile_api/mainapp/admin.py
+++ mobile_api/mainapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone')
+    search_fields = ('name', )
+    list_editable = ('name', 'phone')
+    
+    
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', )
+    search_fields = ('name', )
+    list_editable = ('name', )
+    
+    
+class VisitAdmin(admin.ModelAdmin):
+    list_display = ('date', 'latitude', 'longitude')
+    search_fields = ('shop__worker__name', )
+
+
+admin.site.register(models.Worker, WorkerAdmin)
+admin.site.register(models.Shop, ShopAdmin)
+admin.site.register(models.Visit, VisitAdmin)
--- mobile_api/mainapp/models.py
+++ mobile_api/mainapp/models.py
@@ -5,15 +5,23 @@ class Worker(models.Model):
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
     
+    def __str__(self) -> str:
+        return self.name
+    
 
 class Shop(models.Model):
     name = models.CharField(max_length=255)
     worker = models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True)
     
+    def __str__(self) -> str:
+        return self.name
     
 class Visit(models.Model):
     date = models.DateTimeField()
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    
+    def __str__(self) -> str:
+        return self.id
     
